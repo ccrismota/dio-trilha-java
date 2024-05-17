448b9e48f2d28431ac87df4b55558e6f8f9ab853
ccrismota <ccrismota@gmail.com>
2024-05-17 17:17:47 -0300

    Criação da classe conta com atributos e metodos

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,126 @@
+import java.util.Scanner;
+
+import agencia.Conta;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Minha conta!");
+
+        Scanner sc = new Scanner(System.in).useLocale(java.util.Locale.US);
+
+        Conta conta = new Conta(0125, "2584-8", "Lucas", 1000.00);
+
+        int continuar = 1;
+        while (continuar != 0) {
+
+            System.out.println("------------------------------------------------------------");
+            System.out.println("Por favor, digite o número da operação que deseja realizar!|");
+            System.out.println("0 - Sair                                                   |");
+            System.out.println("1 - Abrir Conta                                            |");
+            System.out.println("2 - Depósito                                               |");
+            System.out.println("3 - Saque                                                  |");
+            System.out.println("4 - Transferência                                          |");
+            System.out.println("5 - Saldo                                                  |");
+            System.out.println("6 - Extrato                                                |");
+            System.out.println("------------------------------------------------------------");
+
+            int op = sc.nextInt();
+            sc.nextLine();
+
+            switch (op) {
+                case 0: {
+                    System.out.println("Obrigado por utilizar nossos serviços! Até a próxima!       |");
+                    System.out.println();
+                    continuar = 0;
+                    break;
+                }
+                case 1: {
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Você digitou a opção 1 - Abrir Conta !                     |");
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Por favor, digite o nome do cliente !                      |");
+                    conta.setNomeCliente(sc.nextLine());
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Por favor, digite o número da Agência !                    |");
+                    conta.setAgencia(sc.nextLine());
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Por favor, digite o número da Conta !                      |");
+                    conta.setNumero(sc.nextInt());
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Por favor, digite o valor do depósito!            |");
+                    conta.setSaldo(sc.nextDouble());
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Olá " + conta.getNomeCliente()
+                            + " obrigado por criar uma conta em nosso banco," + "\n"
+                            + "sua agência é " + conta.getAgencia() + ", conta " + conta.getNumero() + " e seu saldo "
+                            + conta.getSaldo() + " já está" + "\n" + "disponível para saque.");
+                    break;
+                }
+                case 2: {
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Você digitou a opção 2 - Depósito !                        |");
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Por favor, digite o valor do depósito!                     |");
+                    conta.depositar(sc.nextDouble());
+                    System.out.println("------------------------------------------------------------");
+                    break;
+                }
+                case 3: {
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Você digitou a opção 2 - Saque !                           |");
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Por favor, digite o valor do saque !                       |");
+                    conta.sacar(sc.nextDouble());
+                    System.out.println("------------------------------------------------------------");
+                    break;
+                }
+                case 4: {
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Você digitou a opção 3 -Transferência                      |");
+                    System.out.println("------------------------------------------------------------");
+
+                    Conta contaDestino = new Conta(012145, "2584-8", "Lucas", 1000.00);
+
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Por favor, digite o nome do cliente que receberá o valor!  |");
+                    contaDestino.setNomeCliente(sc.nextLine());
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Por favor, digite o número da agência  de transferencia!   |");
+                    contaDestino.setAgencia(sc.nextLine());
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Por favor, digite o número da Conta de transferencia!      |");
+                    contaDestino.setNumero(sc.nextInt());
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println("Por favor, digite o valor da transferência !");
+                    conta.transferir(contaDestino, sc.nextDouble());
+                    System.out.println("------------------------------------------------------------");
+                    break;
+                }
+                case 5: {
+                    System.out.println(conta.toString());
+                    break;
+                }
+                case 6: {
+                    conta.extrato();
+                    break;
+                }
+                default:
+                    System.out.println("Operação inválida");
+                    break;
+            }
+            
+            if (continuar != 0) {
+                System.out.println("Deseja realizar outra operação? 1 - Sim | 0 - Não          |");
+                continuar = sc.nextInt();
+                if (continuar == 0) {
+                    System.out.println("Obrigado por utilizar nossos serviços! Até a próxima!      |");
+                    System.out.println();
+                }
+            } else {
+                System.out.println("Informe uma opção válida! 1(um) ou 0 (zero)                |");
+                continuar = sc.nextInt();
+            }
+        }
+        sc.close();
     }
 }
---
+++ ContaBanco/src/agencia/Conta.java
@@ -0,0 +1,108 @@
+package agencia;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class Conta {
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    // #region getters and setters
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    // #endregion getters and setters
+
+    public Conta(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "------------------------------------------------------------\n" +
+                "Extrato Bancário:\n" +
+                "------------------------------------------------------------\n" +
+                "Número da Conta: " + numero + "\n" +
+                "Agência: " + agencia + "\n" +
+                "Nome do Cliente: " + nomeCliente + "\n" +
+                "Saldo: " + String.format("%.2f", saldo) + "\n" +
+                "data: " + java.time.LocalDateTime.now().format(formatter) + "\n" +
+                "------------------------------------------------------------\n";
+    }
+
+    ArrayList<String> historicoDeOperacoes = new ArrayList<String>();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+
+    public void sacar(double valor) {
+        if (valor > this.saldo) {
+            System.out.println("Saldo insuficiente");
+            return;
+        }
+        this.saldo -= valor;
+        historicoDeOperacoes.add("Saque de R$ " + valor + " realizado em " + java.time.LocalDateTime.now().format(formatter));
+    }
+
+    public void depositar(double valor) {
+        this.saldo += valor;
+        historicoDeOperacoes.add("Depósito de R$ " + valor + " realizado em " + java.time.LocalDateTime.now().format(formatter));
+    }
+
+    public void transferir(Conta contaDestino, double valor) {
+        if (valor > this.saldo) {
+            System.out.println("Saldo insuficiente");
+            return;
+        }
+        this.saldo -= valor;
+        contaDestino.depositar(valor);
+        historicoDeOperacoes.add("\n"
+        + "Transferência de R$ " + valor +"\n"
+        + "\t" + " Para: "+ contaDestino.getNomeCliente() +"\n"
+        + "\t" + " Agencia: "+ contaDestino.getAgencia() +"\n"
+        + "\t" + " Conta: "+ contaDestino.getNumero() +"\n"
+        + "\t" + " Realizada em: " + java.time.LocalDateTime.now().format(formatter) +"\n");
+    }
+
+    public void extrato() {
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Extrato das Operações:                                     |");
+        System.out.println("------------------------------------------------------------");
+        for (String operacao : historicoDeOperacoes) {
+            System.out.println(operacao);
+        }
+        System.out.println(String.format("%.2f", saldo));
+    }
+}
